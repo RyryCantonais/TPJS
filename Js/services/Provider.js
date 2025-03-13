80dee3060eddaf8bc116a91033727716ec5c0ccb
YohannVilcoq <ryrycantonais@gmail.com>
2025-03-13 09:20:19 +0100

    init serveur et json

---
+++ Js/services/Provider.js
@@ -0,0 +1,36 @@
+import { ENDPOINT } from "../config";
+
+export default class Provider{
+    static fetchInventory = async (limit=10) => {
+        const options = {
+            method: "GET",
+            header: {
+                "Content-Type": "application/json"
+            }
+        };
+        try {
+            const response = await fetch(`${ENDPOINT}?_limit=${limit}`, options);
+            const data = await response.json();
+            return data;
+        }
+        catch (error) {
+            console.error(`Error: ${error}`);
+        }
+    }
+
+    static getItem = async (id) => {
+        const options = {
+            method: 'GET',
+            header: {
+                'Content-Type': 'application/json'
+            }
+        };
+        try {
+            const response = await fetch(`${ENDPOINT}/${id}`, options); 
+            const json = await response.json();
+            return json;
+        } catch (err) {
+            console.error('Error getting documents:', err);
+        }
+    }
+}
---
+++ Js/views/Inventory.js
@@ -0,0 +1,22 @@
+import Utils from "../services/Utils.js";
+import Provider from "../services/Provider.js";
+
+export default class Inventory{
+    async render(){
+        let request = Utils.parseRequestURL();
+        let inventory = await Provider.getInventory();
+        let view = /*html*/`
+            <section class="section">
+                <h1>Inventory</h1>
+                <ul>
+                    ${inventory.map(item => /*html*/`
+                        <li>
+                            <a href="#/inventory/${item.id}">${item.name}</a>
+                        </li>
+                    `).join('\n')}
+                </ul>
+            </section>
+        `;
+        return view;
+    }
+}
