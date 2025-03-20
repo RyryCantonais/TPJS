d8ac6ffd63f4460e3ff776f1577533513f558dbc
RyryCantonais <ryrycantonais@gmail.com>
2025-03-20 08:35:23 +0100

    correction

--- js/services/Provider.js
+++ js/services/Provider.js
@@ -116,7 +116,6 @@ export default class Provider{
             console.error('Error getting documents:', err);
         }
     }
-<<<<<<< HEAD
 
     static getInventory = async () => {
         const options = {
@@ -134,6 +133,4 @@ export default class Provider{
             console.error(`Error: ${error}`);
         }   
     }
-=======
->>>>>>> 8dac8106f6b92a0a2dc7c7ebe00f54ef39a62385
 }
