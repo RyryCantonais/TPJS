a42db0e2d8f036c90823660a1f3b5664ae8507d5
RyryCantonais <ryrycantonais@gmail.com>
2025-03-20 08:27:23 +0100

    requêtes

--- js/services/Provider.js
+++ js/services/Provider.js
@@ -1,7 +1,7 @@
 import { ENDPOINT } from "../config.js";
 
 export default class Provider{
-    static getInventory = async (limit=10) => {
+    static getAllArmor = async () => {
         const options = {
             method: "GET",
             header: {
@@ -9,7 +9,58 @@ export default class Provider{
             }
         };
         try {
-            const response = await fetch(`${ENDPOINT}?_limit=${limit}`, options);
+            const response = await fetch(`${ENDPOINT}/armor`, options);
+            const data = await response.json();
+            return data;
+        }
+        catch (error) {
+            console.error(`Error: ${error}`);
+        }   
+    }
+
+    static getArmor = async (id) => {
+        const options = {
+            method: "GET",
+            header: {
+                "Content-Type": "application/json"
+            }
+        };
+        try {
+            const response = await fetch(`${ENDPOINT}/armor/${id}`, options);
+            const data = await response.json();
+            return data;
+        }
+        catch (error) {
+            console.error(`Error: ${error}`);
+        }   
+    }
+
+    static getAllWeapons = async () => {
+        const options = {
+            method: "GET",
+            header: {
+                "Content-Type": "application/json"
+            }
+        };
+        try {
+            const response = await fetch(`${ENDPOINT}/weapons`, options);
+            const data = await response.json();
+            return data;
+        }
+        catch (error) {
+            console.error(`Error: ${error}`);
+        }   
+    }
+
+    static getWeapon = async (id) => {
+        const options = {
+            method: "GET",
+            header: {
+                "Content-Type": "application/json"
+            }
+        };
+        try {
+            const response = await fetch(`${ENDPOINT}/weapons/${id}`, options);
             const data = await response.json();
             return data;
         }
@@ -48,5 +99,38 @@ export default class Provider{
         } catch (err) {
             console.error('Error getting documents:', err);
         }
+    }
+
+    static getMob = async (id) => {
+        const options = {
+            method: 'GET',
+            header: {
+                'Content-Type': 'application/json'
+            }
+        };
+        try {
+            const response = await fetch(`${ENDPOINT}/mobs/${id}`, options);
+            const json = await response.json();
+            return json;
+        } catch (err) {
+            console.error('Error getting documents:', err);
+        }
+    }
+
+    static getInventory = async () => {
+        const options = {
+            method: "GET",
+            header: {
+                "Content-Type": "application/json"
+            }
+        };
+        try {
+            const response = await fetch(`${ENDPOINT}/inventory`, options);
+            const data = await response.json();
+            return data;
+        }
+        catch (error) {
+            console.error(`Error: ${error}`);
+        }   
     }
 }
